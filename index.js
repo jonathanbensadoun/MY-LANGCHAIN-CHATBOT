64c159f027eee068abc1bebf5728db3d946b6d5f
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-06-26 21:26:58 +0200

    change llm to mistral api response mistral code ok

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { AIMessage } from "@langchain/core/messages";
 import { HumanMessage } from "@langchain/core/messages";
+import { InMemoryChatMessageHistory } from "@langchain/core/chat_history";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const apiKey = process.env.OPENAI_API_KEY;
-const organization = process.env.OPENAI_ORGANIZATION_ID;
+
+
+const apiKey = process.env.MISTRAL_API_KEY
+// const organization = process.env.OPENAI_ORGANIZATION_ID;
 
 if (!apiKey) {
   console.error('Erreur : OPENAI_API_KEY n\'est pas définie');
   process.exit(1);
 }
 
-const model = new ChatOpenAI({
-  model: "babbage-002",
-  apiKey: apiKey,
-  organization: organization, // Add the organization parameter
+const model = new ChatMistralAI({
+  model: "mistral-large-latest",
+  temperature: 0,
+  // apiKey:apiKey
 });
+const response = await model.invoke([
+  new HumanMessage({ content: "Hi! I'm Bob" }),
+  new AIMessage({ content: "Hello Bob! How can I assist you today?" }),
+  new HumanMessage({ content: "What's my name?" }),
+]);
+console.log(response)
 
-(async () => {
-  try {
-    const response = await model.invoke([new HumanMessage({ content: "Hi! I'm Jo" })]);
-    console.log(response);
-  } catch (error) {
-    if (error.response && error.response.status === 429) {
-      console.error('Erreur : Vous avez dépassé votre quota. Veuillez vérifier votre plan et vos détails de facturation.');
-    } else if (error.response) {
-      console.error(`Erreur lors de l'appel au modèle : ${error.response.status} ${error.response.statusText}`);
-    } else {
-      console.error('Erreur lors de l\'appel au modèle :', error.message);
+const messageHistories= {};
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    `You are a helpful assistant who remembers all details the user shares with you.`,
+  ],
+  ["placeholder", "{chat_history}"],
+  ["human", "{input}"],
+]);
+
+const chain = prompt.pipe(model);
+
+const withMessageHistory = new RunnableWithMessageHistory({
+  runnable: chain,
+  getMessageHistory: async (sessionId) => {
+    if (messageHistories[sessionId] === undefined) {
+      messageHistories[sessionId] = new InMemoryChatMessageHistory();
     }
-  }
-})();
+    return messageHistories[sessionId];
+  },
+  inputMessagesKey: "input",
+  historyMessagesKey: "chat_history",
+});
